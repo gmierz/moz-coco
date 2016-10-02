48fa1e2bef20fbd465054d737b1f424d7fe1f275
Bradley Kennedy <bk@co60.ca>
2016-10-02 19:20:38 -0400

    Project cleanup and install scripts

---
+++ scripts/install.js
@@ -0,0 +1,34 @@
+
+var fs = require('fs');
+var browserify = require('browserify');
+var less =require('less');
+
+var b = browserify('src/index.js');
+
+
+b.transform({
+    global: true
+}, 'uglifyify');
+
+b.bundle((err, buffer) => {
+  if (err) throw err;
+  fs.writeFile('js/bundle.js', buffer, (err, fd) => {
+    if (err) throw err;
+  }); 
+});
+
+fs.readFile('src/less/custom.less', 'utf8', (err, data) => {
+  if (err) throw err;
+  less.render(data,
+    {
+      compress: true
+    },
+    (err, output) => {
+      if (err) throw err;
+      fs.writeFile('css/custom.css', output.css, (err, fs) => {
+        if (err) throw err;
+      });
+  });
+  
+});
+ 
