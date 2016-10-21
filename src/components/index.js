69d5e87cc97214b9495e9b20c9ec5b70c52c489f
Bradley Kennedy <bk@co60.ca>
2016-10-21 02:28:54 -0400

    Added License text

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,12 @@
+
+/*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+* Author: Bradley Kennedy (bk@co60.ca)
+*/
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Table = require('react-bootstrap/lib/Table');
